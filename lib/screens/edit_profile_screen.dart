c0dcd61675d29f84534a72a5c26cd250f7f33bbc
SabithHaneef <sabith.th.07@gmail.com>
2019-11-21 22:27:46 +0530

    added follow user and unfollow user functionality

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -52,7 +52,7 @@ class _EdiProfileScreenState extends State<EdiProfileScreen> {
   }
 
   _submit() async {
-    if (_formKey.currentState.validate()) {
+    if (_formKey.currentState.validate() && !_isLoading) {
       _formKey.currentState.save();
 
       setState(() {
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -27,4 +27,72 @@ class DatabaseService {
       'timestamp': post.timestamp,
     });
   }
+
+  static void followUser({String currentUserId, String userId}) {
+    // Add userId to current user's following collection
+    followingRef
+        .document(currentUserId)
+        .collection('userFollowing')
+        .document(userId)
+        .setData({});
+
+    // Add current user to userId's followers collection
+    followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .document(currentUserId)
+        .setData({});
+  }
+
+  static void unFollowUser({String currentUserId, String userId}) {
+    // Remove userId from current user's following collection
+    followingRef
+        .document(currentUserId)
+        .collection('userFollowing')
+        .document(userId)
+        .get()
+        .then((doc) {
+      if (doc.exists) {
+        doc.reference.delete();
+      }
+    });
+
+    // Remove current user to userId's followers collection
+    followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .document(currentUserId)
+        .get()
+        .then((doc) {
+      if (doc.exists) {
+        doc.reference.delete();
+      }
+    });
+  }
+
+  static Future<bool> isFollowingUser(
+      {String currentUserId, String userId}) async {
+    DocumentSnapshot followingDoc = await followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .document(currentUserId)
+        .get();
+    return followingDoc.exists;
+  }
+
+  static Future<int> numFollowers(String userId) async {
+    QuerySnapshot followersSnapshot = await followersRef
+        .document(userId)
+        .collection('userFollowers')
+        .getDocuments();
+    return followersSnapshot.documents.length;
+  }
+
+  static Future<int> numFollowing(String userId) async {
+    QuerySnapshot followingSnapshot = await followingRef
+        .document(userId)
+        .collection('userFollowing')
+        .getDocuments();
+    return followingSnapshot.documents.length;
+  }
 }
